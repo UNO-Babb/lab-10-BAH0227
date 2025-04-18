12f5293adf349b3c55099841b4c5f82c9b83890b
BAH0227 <brockahoover@gmail.com>
2025-04-18 05:26:52 +0000

    Finished

--- main.py
+++ main.py
@@ -1,4 +1,27 @@
-#MapPlot.py
+#MatPlot.py
 #Name:
 #Date:
 #Assignment:
+
+import cars
+import pandas
+import matplotlib.pyplot as plt
+car = cars.get_car()
+
+#print(car[0]["Engine Information"]["Engine Statistics"]["Horsepower"])
+#print(car[0]["Identification"]["Model Year"])
+mpgs = []
+horsepowers = []
+for carmodel in car:
+    mpg = carmodel["Fuel Information"]["City mpg"]
+    horsepower = carmodel["Engine Information"]["Engine Statistics"]["Horsepower"]
+    mpgs.append(mpg)
+    horsepowers.append(horsepower)
+    #print(year, horsepower)
+
+df = pandas.DataFrame({"MPG": mpgs,
+                        "Horsepower": horsepowers})
+
+print(df)
+df.plot(kind = 'scatter', x = 'Horsepower', y = 'MPG')
+plt.savefig("output")
